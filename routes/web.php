91255e25e61c75f7602922ef29e7c56d584821a0
n-doka <n-dokai@usen-next.jp>
2022-08-24 10:56:43 +0900

    sagyo1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReserveController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes

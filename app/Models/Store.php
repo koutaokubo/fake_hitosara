efb7cd2070008f924a30255aafdf28db02434613
koutaokubo <kou.tribe22@gmail.com>
2022-08-25 11:43:49 +0900

    change Menu Model

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Reserve;
+use App\Models\Menu;
 
 class Store extends Model
 {
@@ -24,5 +26,13 @@ class Store extends Model
         return $user
             ? (bool)$this->favoriteUsers->where('user_id')->count()
             : false;
+    }
+
+    public function getReserveList() {
+        return $this->belongsToMany(Reserve::class);
+    }
+
+    public function getMenuList() {
+        return $this->belongsToMany(Menu::class);
     }
 }

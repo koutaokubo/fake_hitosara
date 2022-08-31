96e13d95272b4f9b9aded2321f436ddaa9c27493
koutaokubo <kou.tribe22@gmail.com>
2022-08-31 11:08:27 +0900

    予約一覧出力画面変更

--- resources/views/Reserve/index.blade.php
+++ resources/views/Reserve/index.blade.php
@@ -6,12 +6,18 @@
 <div class="text-center">
 @if ($stores != null)
 @if (Auth::check())
-    @foreach ($reserves as $reserve)
+    {{-- @foreach ($reserves as $reserve)
         <p>{{$reserve->id}} {{ Auth::user()->name }}</p>
     @endforeach
     @foreach ($stores as $store)
         <a href="{{ route('store.detail', ['store_id' => $store->id, 'area_id' => $store->area_id]) }}">{{$store->name}}</a>
-    @endforeach
+    @endforeach --}}
+    @for ($i = 0; $i < count($reserves); $i++)
+        <p>{{$reserves[$i]->id}}
+             {{Auth::user()->name }}
+              <a href="{{ route('store.detail', ['store_id' => $stores[$i]->id, 'area_id' => $stores[$i]->area_id]) }}">{{$stores[$i]->name}}</a>
+        </p>
+    @endfor
 @endif
 @else
 <br>
